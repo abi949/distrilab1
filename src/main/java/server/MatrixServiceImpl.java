234a3fe31c692bbd0b75d9876238b0ea1642c289
abi <a.ravindran@se21.qmul.ac.uk>
2024-02-08 21:16:49 +0000

    ..

--- src/main/java/server/MatrixServiceImpl.java
+++ src/main/java/server/MatrixServiceImpl.java
@@ -5,30 +5,48 @@ import com.grpc.matrixmult.MatrixReply;
 import com.grpc.matrixmult.MatrixServiceGrpc.MatrixServiceImplBase;
 
 import io.grpc.stub.StreamObserver;
-public class MatrixServiceImpl extends MatrixServiceImplBase
-{
-	@Override
-	public void addBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
-	{
-		System.out.println("Request received from client:\n" + request);
-		int C00=request.getA00()+request.getB00();
-    		int C01=request.getA01()+request.getB01();
-		int C10=request.getA10()+request.getB10();
-		int C11=request.getA11()+request.getB11();
-		MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
-		reply.onNext(response);
-		reply.onCompleted();
-	}
-	@Override
-    public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
-    {
-        	System.out.println("Request received from client:\n" + request);
-        	int C00=request.getA00()+request.getB00();
-		int C01=request.getA01()+request.getB01();
-		int C10=request.getA10()+request.getB10();
-		int C11=request.getA11()+request.getB11();
-        MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
-        reply.onNext(response);
-        reply.onCompleted();
+
+public class MatrixServiceImpl extends MatrixServiceImplBase {
+
+    @Override
+    public void addBlock(MatrixRequest request, StreamObserver<MatrixReply> replyObserver) {
+        System.out.println("Request received for matrix addition:\n" + request);
+
+        // Perform matrix addition for the specified block
+        int C00 = request.getA00() + request.getB00();
+        int C01 = request.getA01() + request.getB01();
+        int C10 = request.getA10() + request.getB10();
+        int C11 = request.getA11() + request.getB11();
+
+        // Build the response with the result of matrix addition
+        MatrixReply response = MatrixReply.newBuilder()
+                .setC00(C00).setC01(C01)
+                .setC10(C10).setC11(C11)
+                .build();
+
+        // Send the response to the client
+        replyObserver.onNext(response);
+        replyObserver.onCompleted();
+    }
+
+    @Override
+    public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> replyObserver) {
+        System.out.println("Request received for matrix multiplication:\n" + request);
+
+        // Perform matrix multiplication for the specified block
+        int C00 = request.getA00() * request.getB00() + request.getA01() * request.getB10();
+        int C01 = request.getA00() * request.getB01() + request.getA01() * request.getB11();
+        int C10 = request.getA10() * request.getB00() + request.getA11() * request.getB10();
+        int C11 = request.getA10() * request.getB01() + request.getA11() * request.getB11();
+
+        // Build the response with the result of matrix multiplication
+        MatrixReply response = MatrixReply.newBuilder()
+                .setC00(C00).setC01(C01)
+                .setC10(C10).setC11(C11)
+                .build();
+
+        // Send the response to the client
+        replyObserver.onNext(response);
+        replyObserver.onCompleted();
     }
 }
